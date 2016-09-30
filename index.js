0f372ca4d8b77c2cc5424c57a75c6cf005f0d787
Ortex <mywebname@mail.ru>
2016-09-30 09:19:18 +0300

    init

---
+++ index.js
@@ -0,0 +1,219 @@
+"use strict";
+
+const request = require('request-promise');
+const crypto = require('crypto');
+const randomstring = require("randomstring");
+const basicAuth = require('basic-auth');
+const xml = require('xml');
+
+class QiwiShop {
+    constructor(projectId, apiId, apiPassword, notifyPassword) {
+        this.projectId = projectId;        
+        this.apiId = apiId;
+        this.apiPassword = apiPassword;
+        this.notifyPassword = notifyPassword;
+    }
+    
+    _createBillHash() {
+        let hashData = this.projectId + this.apiId + this.notifyPassword + new Date().toString();
+        
+        return crypto.createHash('md5').update(hashData).digest("hex"); 
+    }
+    
+    _createRefundHash() {
+        return randomstring.generate(9);
+    }
+    
+    _setDefaultRequestOptions(options) {
+        options.headers = {
+            'accept': 'text/json',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+        }        
+        options.json = true;        
+        options.auth = {
+            user: this.apiId,
+            pass: this.apiPassword 
+        }
+        
+        return options;
+    }
+    
+    _request(options) {
+        return request(options);
+    }
+    
+    beforeCreateBill(billId, data) {
+        return Promise.resolve();
+    }
+    
+    createBill(data) {
+        let options = {};
+        let billId = this._createBillHash();
+        
+        data.ccy = data.ccy || 'RUB';        
+        data.comment = data.comment || 'bill creation';
+        data.prv_name = data.prv_name || 'merchant';
+        data.pay_source = data.pay_source || 'qw';
+        data.lifetime = data.data || new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 5).toISOString()
+        
+        options.url = `https://api.qiwi.com/api/v2/prv/${this.projectId}/bills/${billId}`;
+        options.method = "PUT";
+        options.form = data;
+        this._setDefaultRequestOptions(options);        
+        
+        return Promise.resolve().then(() => {
+            if(!data.user) {
+                throw new Error('Not found user phone');
+            }
+            
+            if(!data.amount) {
+                throw new Error('Not found amount');
+            }
+            
+            return this.beforeCreateBill(billId, data).then(() => {
+                return this._request(options);
+            })
+        })
+    }
+    
+    getPaymentUrl() {
+        return "https://qiwi.com/order/external/main.action";
+    }
+    
+    getBillStatus(billId) {
+        let options = {};
+        
+        options.url = `https://api.qiwi.com/api/v2/prv/${this.projectId}/bills/${billId}`;
+        options.method = "GET";
+        this._setDefaultRequestOptions(options);
+        
+        return this._request(options);
+    }
+    
+    cancelBill(billId) {
+        let options = {};
+        
+        options.url = `https://api.qiwi.com/api/v2/prv/${this.projectId}/bills/${billId}`;
+        options.method = "PATCH";
+        this._setDefaultRequestOptions(options);
+        
+        return this._request(options);
+    }
+    
+    refundBill(billId, data) {
+        let options = {};
+        
+        if(typeof data != 'object') {
+            data = { amount: data };
+        }
+        
+        options.url = `https://api.qiwi.com/api/v2/prv/${this.projectId}/bills/${billId}/refund/${this._createRefundHash()}`;
+        options.method = "PUT";
+        options.form = data;
+        this._setDefaultRequestOptions(options);
+        
+        return this._request(options);
+    }
+    
+    getRefundStatus(billId, refundId) {
+        let options = {};
+        
+        options.url = `https://api.qiwi.com/api/v2/prv/${this.projectId}/bills/${billId}/refund/${refundId}`;
+        options.method = "GET";
+        this._setDefaultRequestOptions(options);
+        
+        return this._request(options);
+    }
+    
+    checkNotifyAuthBasic(req) {
+        let data = basicAuth(req) || {};
+        
+        return this.notifyPassword && this.projectId && data.name == this.projectId && data.pass == this.notifyPassword;
+    }
+    
+    checkNotifyAuthSignature(req) {
+        let data = req.body || {};
+        let values = [];
+        let params, hash;
+        
+        Object.keys(data).sort().map((key) => {
+            values.push(data[key]);
+        })
+        
+        params = values.join('|');
+        hash = crypto.createHash('sha1').update(params + this.notifyPassword).digest("base64");
+        
+        return hash && req.get('X-Api-Signature') == hash;
+    }
+    
+    createXml(code) {
+        return xml([{ result: [{ result_code: code }] }], { declaration: true });
+    }
+    
+    notify(fn, onError, checkSignature) {
+        if(typeof onError == 'boolean') {
+            checkSignature = onError;
+            onError = undefined;
+        }
+        
+        return (req, res) => {            
+            let checkAuth;
+            
+            let ok = () => {                
+                res.set('Content-Type', 'text/xml');
+                return res.send(this.createXml(0));
+            }
+            
+            let fail = (err, code, meta) => {                
+                res.set('Content-Type', 'text/xml');
+                res.status(500);
+                
+                if(onError) {
+                    onError(err, meta);
+                }
+                
+                return res.send(this.createXml(code || 300));
+            }
+            
+            if(checkSignature) {
+                checkAuth = this.checkNotifyAuthSignature(req);
+            }
+            else {
+                checkAuth = this.checkNotifyAuthBasic(req);
+            }
+            
+            if(!checkAuth) {
+                res.status(401);
+                
+                return fail(new Error('Authentication error'), 150, {
+                    reason: 'authentication',
+                    type: checkSignature? 'signature': 'basic'
+                });
+            }            
+            
+            function callback(err) {
+                if(err) {
+                    return fail(err);
+                }
+                
+                return ok();
+            }
+            
+            if(!fn) {
+                return ok();
+            }
+            
+            let result = fn.call(this, req.body, callback);
+            
+            if(result && typeof result == 'object') {
+                result.then(() => {
+                    ok();
+                }).catch((err) => {
+                    fail(err);
+                })
+            }
+        }
+    }
+}
+
+module.exports = QiwiShop;
---
+++ readme.md
@@ -0,0 +1,107 @@
+# Install 
+`npm install qiwi-shop`
+
+# About
+Qiwi shop rest api module
+
+# Example
+```js
+const QiwiShop = require("qiwi-shop");
+const express = require("express");
+const app = express();
+
+let projectId = "0000001"; // shop id
+let apiId = "0000000001"; // api id
+let apiPassword = "apiapiapi"; // api password
+let notifyPassword = "notifynotifynotify"; // notify password
+
+const qiwi = new QiwiShop(projectId, apiId, apiPassword, notifyPassword);
+
+// Create new bill
+
+// this action is optional
+qiwi.beforeCreateBill = function(billId, data) {
+    // you can save information to db before request e.t.c.
+    // must return promise
+}
+
+// creation request
+app.post('/payments/bill/create/', (req, res, next) => {    
+    qiwi.createBill({
+        user: 'tel:+7910100100',
+        amount: '10',
+        ccy: 'RUB', // optional
+        comment: 'recharge', // optional
+        lifetime: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 5).toISOString(), // optional
+        pay_source: 'qw', // optional
+        prv_name: 'example.com' // optional
+    }).then((result) => {
+        if(!result || !result.response || result.response.result_code != 0) {
+            throw new Error('Qiwi bill creation fail');
+        }
+        
+        // redirect to payment (optional)
+        res.redirect(`${qiwi.getPaymentUrl()}?shop=${qiwi.projectId}&transaction=${result.response.bill.bill_id}&other_options=...`);
+    });
+});
+
+// notify handler 
+let successHandler = (data, callback) => {
+    // data == req.body    
+    // save payment info in db e.t.c    
+    // callback() or return promise
+};
+
+let errorHandler = (err, meta) => {
+    // you can save to a file, db e.t.c.
+    // operation must be synchronous or in the background 
+};
+
+let authenticationBySignature = false; // false = basic authentication
+
+app.post('payments/notify/handler/', qiwi.notify(successHandler, errorHandler, authenticationBySignature));
+
+```
+
+# Description
+You can write custom notify handler, but library version includes data/authentication validation and automatically send all headers in necessary format
+
+# API
+### .constructor(projectId, apiId, apiPassword, notifyPassword)
+all arguments you can find in your qiwi shop account  
+
+### .getPaymentUrl()
+returns qiwi bill creation url
+
+### .beforeCreateBill(billId, data)
+called before bill creation request, but after bill hash creation
+
+### .createBill(data)
+returns promise, create bill, data options must comply with documentation
+
+### .getBillStatus(billId)
+returns promise, get bill status
+
+### .cancelBill(billId)
+returns promise, cancel bill
+
+### .refundBill(billId, data)
+returns promise, refund bill,  data options must comply with documentation
+
+### .getRefundStatus(billId, refundId)
+returns promise, get bill refund status
+
+### .checkNotifyAuthBasic(req)
+check notify basic authentication
+
+### .checkNotifyAuthSignature(req)
+check notify by signature
+
+### .createXml(code)
+create xml string for response
+
+### .notify(fn, onError, checkSignature) 
+qiwi notify handler, it is "connect" middleware
+
+
+
